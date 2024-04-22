9347db908f37e00b21608e95f001a7730e8fe06c
Haozhou Huang <evanhuanghz@hotmail.com>
2024-04-22 17:26:57 -0400

    Youtube Link to mp3

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # FinHub
 
 ## Backend 
-
 ### 1. Create Venv, install requirements, and activate
+.venv\Scripts\activate
+cd backend
 pip install -r requirements.txt
 .venv\Scripts\activate
 ### 2. Create .env file, set environment key 
 OPENAI_API_KEY = paste-my-key-here
 
 
+### 3. ffmpeg install (for link-to-audio)
+#### Windows
+- download from https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z
+- move the ffmpeg.exe and ffprobe.exe to backend/whisper_client/ffmpeg/
+#### Mac (TBD)
 
-Machine Learning Final Project Repo
+
+# Docs
 - Planning page: https://docs.google.com/document/d/1Z7vSNdYFWoE4q9VNosy1tkRNSG_z8pdKKVhqWxOz6K0/edit
 - Sample output page: https://docs.google.com/document/d/1gipkOAjggENA84eLiepr1C2xSv_gs1408usCCPYQ7HA/edit?usp=sharing
-- 
+
--- backend/whisper_client/whisper.py
+++ backend/whisper_client/whisper.py
@@ -8,20 +8,16 @@ load_dotenv()
 # Create a client
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"),)
 
+
+# File path 
 ## Transcribe from English
-def transcribe():
-    audio_file= open("./sample_audio/sample1.mp3", "rb")
+def transcribe(file):
+    audio_file= open(file, "rb")
     transcription = client.audio.transcriptions.create(
     model="whisper-1", 
     file=audio_file
     )
     print(transcription.text)
-
-# Summary
-# Google (company) in the US (location) is undergoing a major reorganization to accelerate AI product development (positive). 
-# CEO Sundar Pichai is shifting the company culture towards more business-focused norms, moving away from its historically open, 
-# socially-involved workplace (negative). This change is emphasized by the recent dismissal of nearly 30 employees protesting a significant Israeli government contract, 
-# highlighting a new, stricter stance on employee activism (negative).
 
 
 ## Translation
@@ -33,5 +29,5 @@ def translate():
     )
     print(translation.text)
     
-
-# transcribe()
+file_path = "./sample_audio/sample1.mp3"
+transcribe(file_path)
