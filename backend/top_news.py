7bbe704818806cb284159369bfef1b51b91f4dfd
Haozhou Huang <evanhuanghz@hotmail.com>
2024-04-28 20:21:01 -0400

    requirement freeze

--- backend/top_news.py
+++ backend/top_news.py
@@ -1,2 +1,18 @@
 from bart.bart import financial_summarizer
 
+
+
+# input news from local file 
+# structure: a csv of title, url, content
+
+
+
+
+# bart summarize -> output summary and entities
+
+
+
+
+
+
+# entity add to (db) -> 
