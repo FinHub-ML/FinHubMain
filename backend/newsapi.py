65e1552ff0d9433abef63cf3524e22cc79eb7b9f
WZ <midas0231@gmail.com>
2024-05-14 17:49:45 -0400

    fixed bug on API usage

--- backend/newsapi.py
+++ backend/newsapi.py
@@ -1,14 +1,14 @@
 from eventregistry import *
 import csv
 # how many pieces to read, tobe modified
-MAX_RESULTS = 1
+MAX_RESULTS = 10
 er = EventRegistry(apiKey = "2f1f8488-4167-4002-a914-03c30828fb5d")
 iter = QueryArticlesIter(
     # queries, tobe modified
     keywords = QueryItems.OR(["Finance", "Stock", "US", "Market", "Economy"]),
     lang = "eng")
 with open('news.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
+    writer = csv.writer(file, quoting=csv.QUOTE_ALL)
     for art in iter.execQuery(er,
             sortBy = "date",
             maxItems = MAX_RESULTS,
@@ -18,7 +18,7 @@ with open('news.csv', mode='w', newline='', encoding='utf-8') as file:
                     categories = True)
             )):
         title = art.get('title', '')
-        body = art.get('body', '')
+        body = art.get('body', '').replace('\n', ' ').replace('\r', ' ')
         url = art.get('url', '')
         writer.writerow([title, url, body])
 
